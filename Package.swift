9f708a6c6f2613b1710356f2697c0ba236da5ed1
tanaka <lemo.nade.engineer.room@gmail.com>
2024-11-04 22:57:05 +0900

    chore: 💚 doccのpluginの入れ忘れ

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
